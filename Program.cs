e419354cb1d1f90ec41e77952d6a811b953e7450
ShitalKadam <shitalkadam1028@gmail.com>
2023-11-01 11:05:04 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassModel_ClassLibrary;
+
+namespace ConsoleApp_Travel_Project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TravelManage travelManage = new TravelManage();
+
+           Menu.ShowMenu();
+         //  travelManage.ViewTravelRequest();
+
+            Console.ReadLine();
+        }
+    }
+}
